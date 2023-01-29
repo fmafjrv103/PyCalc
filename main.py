faba9dd56fa55bbf79b1c69df65089ff6518954e
Fmafjrv103 <fmafjev103@outlook.com>
2023-01-29 16:03:13 -0700

    Starting Addition Option

--- main.py
+++ main.py
@@ -16,5 +16,19 @@ print("          \$$$$$$  |  https://github.com/fmafjrv103/PyCalc ")
 print("           \______/                                   ")
 print("\n")
 print("What do you want to do:\n")
+
+# Printed Menu
 print("1) Addition\n2) Subtraction\n3) Multiplication\n4) Division\n5) Other\n6) Credits")
 
+# Input for Menu
+selectItem = int(input("Pick: ")) # I only want number as it is easier
+
+# Menu
+if selectItem == 1:
+    print("You picked Addition")
+    amountNum = int(input("how many numbers?(2-5): "))# Allows user to pick how many numbers they want to add(only up to 5)
+    if amountNum == 2:
+        index = 0
+        print("You have 2 numbers")
+        while index < 2:
+            num0 = int(input("Enter number: "))
