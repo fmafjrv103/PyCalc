6f824044054045a690b35de376b641834eaf609f
Fmafjrv103 <fmafjev103@outlook.com>
2023-01-29 18:42:01 -0700

    V0.1 Rev 2 of Add option and added Exit option

--- main.py
+++ main.py
@@ -3,12 +3,21 @@
 import numpy as np
 
 # Constants
+# Constants for output
 p = " plus "
 m = " minus "
 d = " divided by "
 m = " times "
 e = " is equal to "
+# Constants for input
 en = "Enter number: "
+n = "How Many Numbers?(1-5): "
+# Warnings
+warn0 = "Do you want to exit?(y or n): "
+warn1 = "Are you sure you want to exit?(y or n): "
+# Errors
+error0 = "Try again"
+error1 = "Invalid Input, Please try again"
 
 # Basic interface
 print("$$$$$$$\             $$$$$$\            $$\           ")
@@ -26,20 +35,20 @@ print("\n")
 print("What do you want to do:\n")
 
 # Printed Menu
-print("1) Addition\n2) Subtraction\n3) Multiplication\n4) Division\n5) Other\n6) Credits")
-
-# Input for Menu
-selectItem = int(input("Pick: ")) # I only want number as it is easier
-
-# Menu
-# Addition
 exit = False
 while exit is False:
-    if selectItem == 1:
-        exit0 = False
-        while exit0 == 0:
+    print("1) Addition\n2) Subtraction\n3) Multiplication\n4) Division\n5) Other\n6) Credits\n7) Exit")# Menu
+
+    # Input for Menu
+    selectItem = int(input("Pick: ")) # I only want number as it is easier
+
+    # Menu
+    # Addition
+    exit0 = False
+    while exit0 is False:
+        if selectItem == 1:
             print("You picked Addition")
-            amountNum = int(input("how many numbers?(2-5): "))# Allows user to pick how many numbers they want to add(only up to 5)
+            amountNum = int(input(n))# Allows user to pick how many numbers they want to add(only up to 5)
             num = "You have ",amountNum," Numbers"# Constant for numbers
             if amountNum == 2:
                 numArray = [0,0]# Array used to store numbers inputed by user
@@ -50,37 +59,74 @@ while exit is False:
                     index += 1
                 out = numArray[0] + numArray[1]
                 print(numArray[0],p,numArray[1],e,out,"\n")
-                exit0 = input("Would you like to exit(y or n): ")
-                if exit0 == "y":
-                    exit0 = 1
-                else:
-                    exit0 = 0
+                while exit0 is False:
+                    out0 = input(warn0)
+                    if out0 == "y":
+                        exit0 = True
+                    elif out0 == "n":
+                        break
+                    else:
+                        print(error0)
             elif amountNum == 3:
                 numArray = [0,0,0]
                 index = 0
+                print(num)
                 while index < len(numArray):
-                    print(num)
                     numArray[index] = int(input(en))
                     index += 1
                 out = numArray[0]+numArray[1]+numArray[2]
-                print(numArray[0],p,numArray[1],p,numArray[2],e,out)
+                print(numArray[0],p,numArray[1],p,numArray[2],e,out,"\n")
+                while exit0 is False:
+                    out0 = input(warn0)
+                    if out0 == "y":
+                        exit0 = True
+                    elif out0 == "n":
+                        break
+                    else:
+                        print(error0)
             elif amountNum == 4:
                 numArray = [0,0,0,0]
                 index = 0
+                print(num)
                 while index < len(numArray):
-                    print(num)
                     numArray[index] = int(input(en))
                     index += 1
                 out = numArray[0]+numArray[1]+numArray[2]+numArray[3]
-                print(numArray[0],p,numArray[1],p,numArray[2],p,numArray[3],e,out)
+                print(numArray[0],p,numArray[1],p,numArray[2],p,numArray[3],e,out,"\n")
+                while exit0 is False:
+                    out0 = input(warn0)
+                    if out0 == "y":
+                        exit0 = True
+                    elif out0 == "n":
+                        break
+                    else:
+                        print(error0)
             elif amountNum == 5:
                 numArray = [0,0,0,0,0]
                 index = 0
+                print(num)
                 while index < len(numArray):
-                    print(num)
                     numArray[index] = int(input(en))
                     index += 1
                 out = numArray[0]+numArray[1]+numArray[2]+numArray[3]+numArray[4]
-                print(numArray[0],p,numArray[1],p,numArray[2],p,numArray[3],p,numArray[4],e,out)
+                print(numArray[0],p,numArray[1],p,numArray[2],p,numArray[3],p,numArray[4],e,out,"\n")
+                while exit0 is False:
+                    out0 = input(warn0)
+                    if out0 == "y":
+                        exit0 = True
+                    elif out0 == "n":
+                        break
+                    else:
+                        print(error0)
             else:
-                print("Invalid Input, Please try again")
+                print(error1)
+
+# Exit
+        elif selectItem == 7:
+                out1 = input(warn1)
+                if out1 == "y":
+                    exit = True
+                elif out1 == "n":
+                    break
+                else:
+                    print(error0)
